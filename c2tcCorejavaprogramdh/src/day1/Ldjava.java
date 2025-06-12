e7f0b3360a77af4bbfdbec8027b0559e4d0f5a4c
ACT-C1-18 <ACT-C1-18@DESKTOP-I4SCAC5>
2025-06-12 11:53:52 +0530

    Today i done my Addition

--- c2tcCorejavaprogramdh/src/day1/Ldjava.java
+++ c2tcCorejavaprogramdh/src/day1/Ldjava.java
@@ -3,8 +3,12 @@ package day1;
 public class Ldjava {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to the java session");
+		int a,b,c;
+		a=Integer.parseInt(args[0]);
+		b=Integer.parseInt(args[0]);
+		c=a+b;
+		System.out.println("Addition:"+c);
 
-	}
+	}   
 
 }
