6fef38c4586ba42dce9f8a4054b598b40f39bf5b
ACT-C1-18 <ACT-C1-18@DESKTOP-I4SCAC5>
2025-06-13 12:31:05 +0530

    This is my Entity class program

---
+++ c2tcCorejavaprogramdh/src/EntityClass/DemoStudent.java
@@ -0,0 +1,16 @@
+package EntityClass;
+
+public class DemoStudent {
+
+	public static void main(String[] args) {
+		Student st = new Student();
+		st.setAge(20);
+		st.setId(101);
+		st.setName("DEVA");
+		System.out.println("ID:"+st.getAge());
+		
+	
+
+	}
+
+}
