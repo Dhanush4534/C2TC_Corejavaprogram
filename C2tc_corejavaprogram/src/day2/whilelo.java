eeafb216e00390a78543b4143deb8d3a8b7e9d9d
Lenovo <Lenovo@DESKTOP-QBU23M7>
2025-06-15 21:29:02 +0530

    whileloop day2

--- C2tc_corejavaprogram/src/day2/whilelo.java
+++ C2tc_corejavaprogram/src/day2/whilelo.java
@@ -3,13 +3,11 @@ package day2;
 public class whilelo {
 
 	public static void main(String[] args) {
-				int i=99;
-				while(i<=100)
-				{
-					System.out.println(i);
-					i++;
-				}
-			
+		int i = 99;
+		while (i <= 100) {
+			System.out.println(i);
+			i++;
+		}
 
 	}
 
