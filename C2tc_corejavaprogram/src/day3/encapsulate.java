84826af1e26e63f4f30fe241e5dc32b29355a215
Lenovo <Lenovo@DESKTOP-QBU23M7>
2025-06-15 22:52:34 +0530

    this is day three

--- C2tc_corejavaprogram/src/day3/encapsulate.java
+++ C2tc_corejavaprogram/src/day3/encapsulate.java
@@ -2,9 +2,17 @@ package day3;
 
 public class encapsulate {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public static void main(String[] args) {
+		//object creation 
+		OopsConceptDemo obj = new OopsConceptDemo();
+		obj.setSerialNum(101);
+		obj.setName("Pooja");
+		obj.setAge(20);
+		System.out.println(obj);
+		
+	}
+
 
 	}
 
-}
+
